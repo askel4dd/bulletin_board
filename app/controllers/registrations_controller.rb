6079d47498a532394de05e78981178bdd4e63712
dimon <its2exp@gmail.com>
2015-04-01 01:19:39 +0300

    Minor fix in registrations controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confiramtion, :login, :fname, :lname, :birthday, :address, :city, :zip, :state) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :login, :fname, :lname, :birthday, :address, :city, :zip, :state) }
     end
 end
