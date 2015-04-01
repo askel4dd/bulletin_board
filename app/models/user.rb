0027b1e53b527017999c38805be0aebf845a1f2e
dimon <its2exp@gmail.com>
2015-04-01 16:46:35 +0300

    Set role for new users to generic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ActiveRecord::Base
             :address, :city, :zip, :state, presence: true
   belongs_to :role
   has_many :adverts
-
+  before_save :set_role
+  
+  def set_role
+    self.role = Role.find_by(name: "Generic") if self.role.nil?
+  end
   def role?(role)
     return false if self.role == nil
     self.role.name.downcase.to_sym == role
