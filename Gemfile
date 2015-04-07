2d1a8a8f93e840a1a3dbac5d46dbc0ca58e07702
dimon <its2exp@gmail.com>
2015-04-08 00:24:04 +0300

    Redcarpet markup.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'redcarpet'
 gem 'best_in_place'
 gem 'fog'
 gem 'carrierwave'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+  def markdown(text)
+    render_options = {
+          #Remove HTML tags
+          filter_html: true,
+          # <br /> tags in paragraphs where are newlines
+          hard_wrap: true,
+          # hash for extra link options
+          link_attributes: { rel: 'nofollow'},
+          #Prettify
+          prettify: true
+    }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+
+    extensions = {
+          #will parse links without need of enclosing them
+          autolink:           true,
+          # blocks delimited with 3 ` or ~ will be considered as code block.
+          # No need to indent.  You can provide language name too.
+          # ```ruby
+          # block of code
+          # ```
+          fenced_code_blocks: true,
+          # will ignore standard require for empty lines surrounding HTML blocks
+          lax_spacing:        true,
+          # will not generate emphasis inside of words, for example no_emph_no
+          no_intra_emphasis:  true,
+          # will parse strikethrough from ~~, for example: ~~bad~~
+          strikethrough:      true,
+          # will parse superscript after ^, you can wrap superscript in ()
+          superscript:        true
+          # will require a space after # in defining headers
+          # space_after_headers: true
+    }
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
